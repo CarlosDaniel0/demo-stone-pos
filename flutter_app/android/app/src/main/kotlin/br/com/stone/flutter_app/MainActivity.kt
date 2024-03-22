52180224ecffb80a7c59b8ae0e45c9b742edf1c9
CarlosDaniel0 <51516076+CarlosDaniel0@users.noreply.github.com>
2024-03-22 10:23:40 -0300

    Atualização de Versão Flutter para testes

---
+++ flutter_app/android/app/src/main/kotlin/br/com/stone/flutter_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.com.stone.flutter_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ flutter_app/lib/main.dart
@@ -0,0 +1,87 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(title: 'DeepLink Demo'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  static const platformMethodChannel =
+      const MethodChannel("mainDeeplinkChannel");
+  String deeplinkResult = "";
+
+  Future<Null> _sendDeeplink() async {
+    String _message = "";
+    try {
+      int amount = 100;
+      bool editableAmount = false; //true, false
+      int? installmentCount; //número de 2 a 18
+      String transactionType = "DEBIT"; //DEBIT, CREDIT, VOUCHER
+      String? installmentType; //MERCHANT, ISSUER, NONE
+      int? orderId;
+      String returnScheme = "flutterdeeplinkdemo";
+
+      await platformMethodChannel.invokeMethod('sendDeeplink', {
+        "amount": amount,
+        "editableAmount": editableAmount,
+        "installmentCount": installmentCount,
+        "transactionType": transactionType,
+        "installmentType": installmentType,
+        "orderId": orderId,
+        "returnScheme": returnScheme
+      });
+    } on PlatformException catch (e) {
+      _message = "Erro ao enviar deeplink: ${e.message}.";
+    }
+    setState(() {
+      deeplinkResult = _message;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(widget.title),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              ElevatedButton(
+                onPressed: _sendDeeplink,
+                child: const Text('Enviar deeplink',
+                    style: TextStyle(fontSize: 20)),
+              ),
+              Text(deeplinkResult),
+            ],
+          ),
+        ));
+  }
+}
